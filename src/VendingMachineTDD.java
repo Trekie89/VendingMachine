136afff10250dedb80ccd21408010d59b3924b1a
Samantha Behn <horses4life2011@gmail.com>
2017-04-28 10:33:49 -0400

    cleaned up TDD portion

--- src/VendingMachineTDD.java
+++ src/VendingMachineTDD.java
@@ -18,46 +18,23 @@ public class VendingMachineTDD {
     }
 
     @Test
-    public void customerInsertsNickle() {
+    public void customerInsertsACoin() {
         VendingMachine vendingMachine = new VendingMachine();
         assertEquals("0.05", vendingMachine.coins(Coins.Nickle));
-    }
-
-    @Test
-    public void customerInsertsDime() {
-        VendingMachine vendingMachine = new VendingMachine();
         assertEquals("0.10", vendingMachine.coins(Coins.Dime));
-    }
-
-    @Test
-    public void customerInsertsQuarter() {
-        VendingMachine vendingMachine = new VendingMachine();
         assertEquals("0.25", vendingMachine.coins(Coins.Quarter));
-    }
-
-    @Test
-    public void customerInsertsPenny() {
-        VendingMachine vendingMachine = new VendingMachine();
         assertEquals("not valid coin", vendingMachine.coins(Coins.Penny));
     }
 
     @Test
-    public void customerPushesButtonForCola() {
+    public void customerPushesButtonForProduct() {
         VendingMachine vendingMachine = new VendingMachine();
         assertEquals("Cola", vendingMachine.button(Buttons.Cola));
-    }
-
-    @Test
-    public void customerPushesButtonForChips() {
-        VendingMachine vendingMachine = new VendingMachine();
         assertEquals("Chips", vendingMachine.button(Buttons.Chips));
-    }
-
-    @Test
-    public void customerPushesButtonForCandy() {
-        VendingMachine vendingMachine = new VendingMachine();
         assertEquals("Candy", vendingMachine.button(Buttons.Candy));
     }
+
+
 
 
 
